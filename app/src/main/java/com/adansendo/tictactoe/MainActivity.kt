f5247ca9195efc98f1c37f1f3065f83c5dba1d40
Adan Turcios <adanturcios123@gmail.com>
2025-07-19 09:55:38 -0600

    logic for 1st screen

--- app/src/main/java/com/adansendo/tictactoe/MainActivity.kt
+++ app/src/main/java/com/adansendo/tictactoe/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.adansendo.tictactoe
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +15,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.play)
+        var ops = findViewById<RadioGroup>(R.id.opGroup)
+        val chk = findViewById<CheckBox>(R.id.turn)
+        btn.setOnClickListener {
+            val intent1 = Intent(this,Game::class.java)
+            var selected = ops.checkedRadioButtonId;
+
+            if(selected == R.id.op1)
+               intent.putExtra("character",R.drawable.cross)
+                else
+                intent.putExtra("character",R.drawable.circle)
+
+            intent1.putExtra("gofirst",chk.isSelected);
+            startActivity(intent1)
+        }
     }
 }
